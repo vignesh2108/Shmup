c603cf6b6288e731b77b44823b3bd786ed6b11b0
Vignesh Karthikeyan <vignesh2108@gmail.com>
2020-10-07 13:55:08 -0600

    Added MyShmupRemix Folder

---
+++ MyShmupRemix/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   
+   // private void OnCollisionEnter2D(Collision2D collision)
+   // {
+   //    Destroy(gameObject);
+   // }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+      Destroy(gameObject);
+   }
+}
---
+++ MyShmupRemix/Assets/Scripts/enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject hitEffect;
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {  
+    //     GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+    //     Destroy(effect, 0.5f);
+    //     Destroy(gameObject);
+    // }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "bullet")
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            // Destroy(effect, 0.5f);
+            Destroy(gameObject);
+        }
+    }    
+}
